0f11ed91a703da8c48647d7d192cb10b50c5c6f6
Prasad Bhokare <prasad.bhokare@bentley.com>
2019-10-11 22:24:13 +0530

    Adding generic cache

---
+++ GenericCachedRepository/GenericCache.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GenericCachedRepository
+{
+    public class GenericCache<TKey, TValue, TIReadRepository, TIWriteRepository>
+    {
+        private readonly TIReadRepository _primaryReadRepository;
+        private readonly TIReadRepository _secondaryReadRepository;
+        private readonly TIWriteRepository _primaryWriteRepository;
+        private readonly Func<TIReadRepository, TKey, TValue> _getDataFunc;
+        private readonly Action<TIWriteRepository, TKey, TValue> _setDataFunc;
+
+        public GenericCache(TIReadRepository primaryReadRepository,
+                            TIReadRepository secondaryReadRepository,
+                            TIWriteRepository primaryWriteRepository,
+                            Func<TIReadRepository, TKey, TValue> getDataFunc,
+                            Action<TIWriteRepository, TKey, TValue> setDataFunc)
+        {
+            _primaryReadRepository = primaryReadRepository;
+            _secondaryReadRepository = secondaryReadRepository;
+            _primaryWriteRepository = primaryWriteRepository;
+            _getDataFunc = getDataFunc;
+            _setDataFunc = setDataFunc;
+        }
+
+        public virtual TValue GetDataByKey(TKey key)
+        {
+            var value = GetDataFromPrimaryResource(key);
+            if (null == value)
+            {
+                value = GetDataFromSecondaryResource(key);
+
+                if (value != null)
+                    SetDataPrimaryResource(key, value);
+            }
+            return value;
+        }
+
+        private void SetDataPrimaryResource(TKey key, TValue value)
+        {
+            _setDataFunc.Invoke(_primaryWriteRepository, key, value);
+        }
+
+        private TValue GetDataFromPrimaryResource(TKey key)
+        {
+            return _getDataFunc.Invoke(_primaryReadRepository, key);
+        }
+
+        private TValue GetDataFromSecondaryResource(TKey key)
+        {
+            return _getDataFunc.Invoke(_secondaryReadRepository, key);
+        }
+    }
+}
---
+++ GenericCahceUnitTests/CacheUnitTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using GenericCachedRepository;
+using Moq;
+using NUnit.Framework;
+
+namespace GenericCacheUnitTests
+{
+    [TestFixture]
+    public class CacheUnitTests
+    {
+        private Mock<IReadRepository> _primaryReadRepo;
+        private Mock<IReadRepository> _secondaryReadRepo;
+        private Mock<IWriteRepository> _primaryWriteRepo;
+        private GenericCache<Guid, Value, IReadRepository, IWriteRepository> _cache;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            _primaryReadRepo = new Mock<IReadRepository>(MockBehavior.Strict);
+            _secondaryReadRepo = new Mock<IReadRepository>(MockBehavior.Strict);
+            _primaryWriteRepo = new Mock<IWriteRepository>(MockBehavior.Strict);
+            _cache = new GenericCache<Guid, Value, IReadRepository, IWriteRepository>
+                        (_primaryReadRepo.Object,
+                        _secondaryReadRepo.Object,
+                        _primaryWriteRepo.Object,
+                        (repo, k) => repo.GetValue(k),
+                        (repo, k, v) => repo.SetValue(k, v));
+        }
+
+        [TestCase("name1")]
+        [TestCase("name2")]
+        [Test]
+        public void GetData_FromPrimarySource_ExpectsData(string name)
+        {
+            //Arrange
+            var key = Guid.NewGuid();
+            var value = new Value() { Name = name };
+            _primaryReadRepo.Setup(r => r.GetValue(key)).Returns(value);
+            _secondaryReadRepo.Setup(r => r.GetValue(key)).Returns(value);
+
+            //Act
+            Value result = _cache.GetDataByKey(key);
+
+            //Assert
+            Assert.AreEqual(name, result.Name);
+        }
+
+        [TestCase("name3")]
+        [TestCase("name4")]
+        [Test]
+        public void GetData_FromSecondarySource_ExpectsDataAndUpdatePrimary(string name)
+        {
+            //Arrange
+            var key = Guid.NewGuid();
+            var value = new Value() { Name = name };
+            _primaryReadRepo.Setup(r => r.GetValue(key)).Returns((Value)null);
+            _secondaryReadRepo.Setup(r => r.GetValue(key)).Returns(value);
+            _primaryWriteRepo.Setup(r => r.SetValue(key, value));
+
+            //Act
+            Value result = _cache.GetDataByKey(key);
+
+            //Assert
+            Assert.AreEqual(name, result.Name);
+            _primaryWriteRepo.Verify(r => r.SetValue(key, value), Times.Once);
+        }
+
+        [Test]
+        public void GetData_NotFoundInEitherSource_ExpectsNull()
+        {
+            //Arrange
+            var key = Guid.NewGuid();
+            _primaryReadRepo.Setup(r => r.GetValue(key)).Returns((Value)null);
+            _secondaryReadRepo.Setup(r => r.GetValue(key)).Returns((Value)null);
+
+            //Act
+            Value result = _cache.GetDataByKey(key);
+
+            //Assert
+            Assert.IsNull(result);
+            _primaryWriteRepo.Verify(r => r.SetValue(It.IsAny<Guid>(), It.IsAny<Value>()), Times.Never);
+        }
+    }
+
+    public interface IReadRepository
+    {
+        Value GetValue(Guid key);
+    }
+
+    public interface IWriteRepository
+    {
+        void SetValue(Guid key, Value value);
+    }
+
+
+    public class Value
+    {
+        public string Name { get; set; }
+    }
+}
